ff2d13abd82030c65bbf3f049567a76741de0d44
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2021-08-05 14:10:15 -0400

    Initial

---
+++ PlotAlphaDiversityPlugin.cpp
@@ -0,0 +1,40 @@
+#include "PluginManager.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "PlotAlphaDiversityPlugin.h"
+
+void PlotAlphaDiversityPlugin::input(std::string file) {
+ inputfile = file;
+ std::ifstream ifile(inputfile.c_str(), std::ios::in);
+ while (!ifile.eof()) {
+   std::string key, value;
+   ifile >> key;
+   ifile >> value;
+   parameters[key] = value;
+ }
+ 
+}
+
+void PlotAlphaDiversityPlugin::run() {
+   
+}
+
+void PlotAlphaDiversityPlugin::output(std::string file) {
+//beta_diversity.py -i filtered_otu_table.biom -m euclidean,weighted_unifrac,unweighted_unifrac -t rep_set.tre -o beta_div
+   std::string command = "alpha_rarefaction.py ";
+ command += "-i "+std::string(PluginManager::prefix())+"/"+parameters["biomfile"];
+ command += " -m "+std::string(PluginManager::prefix())+"/"+parameters["mapping"];
+ command += " -e "+parameters["depth"];
+ for (int i = 0; i < metrics.size(); i++) {
+    command += metrics[i];
+    if (i != metrics.size()-1){command += ",";}
+    else {command += " ";}
+ }
+ command += " -t "+std::string(PluginManager::prefix())+"/"+parameters["tree"]+" ";
+ command += "-f -o "+file+"; cp "+file+"/alpha_div_collated/* "+file+"/..";//; export PYTHONPATH=OLDPATH";
+ std::cout << command << std::endl;
+
+ system(command.c_str());
+}
+
+PluginProxy<PlotAlphaDiversityPlugin> PlotAlphaDiversityPluginProxy = PluginProxy<PlotAlphaDiversityPlugin>("PlotAlphaDiversity", PluginManager::getInstance());
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # PlotAlphaDiversity
-Compute and plot alpha-diversity for sample sets, using multiple metrics (Caporaso et al, 2010)
+# Language: C++
+# Input: TXT
+# Output: Directory
+# Tested with: PluMA 1.0, GCC 4.8.4
+# Dependency: Qiime 1.9.1, Python 2.7
+
+PluMA plugin to plot alpha-diversity of a set of samples using Qiime (Caporaso et al, 2010).
+
+The plugin accepts as input a TXT file containing lines with keywords and values (whitespaced-delimited):
+biomfile: OTUs and abundances (BIOM)
+tree: Phylogenetic tree (TRE)
+mapping: Metadata (CSV)
+depth: Sampling Depth (integer)
+
+
+The output for this plugin is a directory, where plots will be stored.
+
+Note: This plugin works with Qiime 1.  
